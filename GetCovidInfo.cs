d79ecab060e02e1745a0aedfb316fea1324adda9
Stolzenberg <timstolzenberg@outlook.de>
2022-02-03 16:27:01 +0100

    Create new repository for application project

---
+++ GetCovidInfo.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace Stolzenberg.Covid
+{
+    public class GetCovidInfo
+    {
+        private readonly ILogger<GetCovidInfo> _logger;
+        private readonly HttpClient _client = new HttpClient();
+
+        public GetCovidInfo(ILogger<GetCovidInfo> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("GetCovidInfo")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseBody = "";
+
+            
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return new OkObjectResult(responseBody);
+        }
+    }
+}
+
